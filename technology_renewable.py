fe019192530149dd5436842df0d84f16c4b2d447
Marco Lombardini <71824347+MarcoL-LAM@users.noreply.github.com>
2020-10-28 19:04:41 +0100

    Add files via upload

---
+++ technology_renewable.py
@@ -0,0 +1,7 @@
+#Elenco technology da utilizzare nel FacetGrid
+
+technology_list_renewable = ['Bioenergy and renewable waste',
+'Differently categorized renewable energy sources',
+'Other bioenergy and renewable waste',
+'Renewable energy sources',
+]
